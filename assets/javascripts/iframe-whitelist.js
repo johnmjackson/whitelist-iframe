3b00910b5a96039389aeca400e9a99c39b8d1ca9
johnmjackson <john.jackson@flowxo.com>
2016-03-11 19:00:55 +0000

    Update iframe-whitelist.js

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -1,7 +1,3 @@
 (function() {
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.youtube\.com\/embed\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/flowxo\.com\/share\/.+/i);
 })();
-
